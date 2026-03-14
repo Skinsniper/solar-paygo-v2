8a9bd21a72187fa90fcec16d3c6ebc90bb47bd16
Skinsniper <deorukundo615@gmail.com>
2026-03-14 18:38:00 +0000

    Add Prettier, ESLint config, VS Code settings, production payment UI

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import eslintConfigPrettier from 'eslint-config-prettier'; // 1. Import it
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  eslintConfigPrettier, // 2. Add it LAST to disable conflicting rules
+];
